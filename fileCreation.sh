75db50b05a3ff4a739034c39208e3bef3445ce34
GerardoA01701474 <58478243+GerardoA01701474@users.noreply.github.com>
2023-11-23 22:42:39 +0000

    file creation modified

--- fileCreation.sh
+++ fileCreation.sh
@@ -2,8 +2,19 @@ count=10
 mkdir text_files/ &> /dev/null
 declare -a array=("Gerardo" "Jose" "Maria" "Gerardo" "Jose" "Maria" "Gerardo" "Jose" "Maria" "Gerardo" "Jose" "Maria")
 
-while [ $count -gt 0 ]; do
-    touch text_files/file$count.txt
-    echo ${array[$count]} | cat >> text_files/file$count.txt
-    count=$((count-1))
-done
+############ First ##################
+#while [ $count -gt 0 ]; do
+   # touch text_files/file$count.txt
+  #  echo ${array[$count]} | cat >> text_files/file$count.txt
+ #   count=$((count-1))
+#done
+
+############## Second ###################
+#for filename in File{1..10}; do
+   # touch text_files/"$filename".txt
+  #  echo ${array[$count]} | cat >> text_files/"$filename".txt
+ #   count=$((count-1))
+#done
+
+################ Third #####################
+touch text_files/file{1..10}
